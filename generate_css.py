859cc1f9e227224d554edfb3bebeb9ca11319829
Kevin Tang <kevintang2023@gmail.com>
2020-09-10 18:30:27 -0400

    Script to generate checkbox CSS

---
+++ generate_css.py
@@ -0,0 +1,13 @@
+import json
+
+if __name__ == '__main__':
+    s = "#{}-f:checked+label:after {{\n    top: 0;\n    width: 20px;\n    height: 20px;\n    border: 2px solid {} !important;\n    background-color: {} !important;\n    z-index: 0;\n}}\n"
+    
+    with open('orgs.json') as f:
+        orgs = json.load(f)
+
+    print(orgs)
+
+    for org in orgs:
+        print(s.format(org['className'], org['color'], org['color']));
+
